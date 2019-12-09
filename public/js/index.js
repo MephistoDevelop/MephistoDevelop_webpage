c263ece8e42334659b0f58991ac19bdc42b4b1f2
Cristian <inesherald@gmail.com>
2019-12-09 06:42:24 -0600

    Add animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,26 @@
-var text = document.getElementById("text");
-var newDom = "";
-var animationDelay = 6;
+window.onload = function() {
+  var text2 = document.getElementById("texto");
+  var newDom = "";
+  var newDom2 = "";
+  var animationDelay = 6;
+  for (let i = 0; i < text.innerText.length; i++) {
+    newDom +=
+      '<span class="char">' +
+      (text.innerText[i] == " " ? "&nbsp;" : text.innerText[i]) +
+      "</span>";
+  }
+  for (let i = 0; i < text2.innerText.length; i++) {
+    newDom2 +=
+      '<span class="char">' +
+      (text2.innerText[i] == " " ? "&nbsp;" : text2.innerText[i]) +
+      "</span>";
+  }
 
-for (let i = 0; i < text.innerText.length; i++) {
-  newDom +=
-    '<span class="char">' +
-    (text.innerText[i] == " " ? "&nbsp;" : text.innerText[i]) +
-    "</span>";
-}
+  text.innerHTML = newDom;
+  text2.innerHTML = newDom2;
+  var length = text.children.length;
 
-text.innerHTML = newDom;
-var length = text.children.length;
-
-for (let i = 0; i < length; i++) {
-  text.children[i].style["animation-delay"] = animationDelay * i + "ms";
-}
+  for (let i = 0; i < length; i++) {
+    text.children[i].style["animation-delay"] = animationDelay * i + "ms";
+  }
+};
