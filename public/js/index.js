8ee4d25669be21befb4436a647260c6fcc143115
Cristian <inesherald@gmail.com>
2019-12-11 06:04:41 -0600

    Add styles and navbar

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,3 @@
-window.onload = function() {
-  var text2 = document.getElementById("texto");
-  var newDom = "";
-  var newDom2 = "";
-  var animationDelay = 6;
-  for (let i = 0; i < text.innerText.length; i++) {
-    newDom +=
-      '<span class="char">' +
-      (text.innerText[i] == " " ? "&nbsp;" : text.innerText[i]) +
-      "</span>";
-  }
-  for (let i = 0; i < text2.innerText.length; i++) {
-    newDom2 +=
-      '<span class="char">' +
-      (text2.innerText[i] == " " ? "&nbsp;" : text2.innerText[i]) +
-      "</span>";
-  }
-
-  text.innerHTML = newDom;
-
-  text2.innerHTML = newDom2;
-  var length = text.children.length;
-
-  for (let i = 0; i < length; i++) {
-    text.children[i].style["animation-delay"] = animationDelay * i + "ms";
-  }
-};
+function changeDefOver(e) {
+  e.target.classList.toggle("opacity-toggle");
+}
