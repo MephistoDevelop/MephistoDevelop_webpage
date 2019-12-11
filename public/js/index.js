a8c805393d9de8fc5073e03923ea8b011e30a2dc
Cristian <inesherald@gmail.com>
2019-12-11 07:50:32 -0600

    Navbar animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,14 @@
 function changeDefOver(e) {
   e.target.classList.toggle("opacity-toggle");
 }
+var prevScrollpos = 0;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar-main").style.top = "0px";
+  } else {
+    document.getElementById("navbar-main").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
