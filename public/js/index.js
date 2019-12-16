0985c3341e1239b67313d8ba6892c467ee94212e
Cristian <inesherald@gmail.com>
2019-12-16 06:59:39 -0600

    Add return top function

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,8 @@
+function gotop() {
+  var body = $("html, body");
+  body.stop().animate({ scrollTop: 0 }, 500, "swing", function() {});
+}
+
 function changeDefOver(e) {
   e.target.classList.toggle("opacity-toggle");
 }
@@ -7,8 +12,10 @@ window.onscroll = function() {
 
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar-main").style.top = "0px";
+    document.getElementById("backtop").style.marginLeft = "230px";
   } else {
     document.getElementById("navbar-main").style.top = "-50px";
+    document.getElementById("backtop").style.marginLeft = "180px";
   }
   prevScrollpos = currentScrollPos;
 };
