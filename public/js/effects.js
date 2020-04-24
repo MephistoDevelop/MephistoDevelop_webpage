01d4aea95058af0e28b66832ac966546b33490f4
MephistoDevelop <inesherald@gmail.com>
2020-04-24 03:11:40 -0500

    Fix mobile navigaion

--- public/js/effects.js
+++ public/js/effects.js
@@ -149,21 +149,4 @@ jQuery(document).ready(function( $ ) {
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
   });
 
-  // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
 });
